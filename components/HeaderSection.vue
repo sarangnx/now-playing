e01bb3fd0bd4e133ba9c6f19b4b821d91a6bdbd0
Sarang <sarang98divakaran@gmail.com>
2020-11-12 17:41:05 +0530

    🌟 feat: added header section component

---
+++ components/HeaderSection.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="header">
+        <div class="text">
+            <span>Showcase Your Taste in Music</span>
+            <span>on Your Github README.md</span>
+        </div>
+        <div class="cta">
+            <button>CONNECT SPOTIFY</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.header {
+    display: grid;
+    justify-content: center;
+    row-gap: 1.5rem;
+    margin-top: 4rem;
+    .text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-weight: bold;
+        line-height: 2;
+    }
+
+    .cta > button {
+        background: #1db954;
+        border: 0px;
+        outline: none;
+        color: white;
+        padding: 0.8rem 4rem;
+        border-radius: 30px;
+        font-weight: bold;
+        font-size: 0.8rem;
+        letter-spacing: 4px;
+        transition: 0.5s;
+        &:hover {
+            cursor: pointer;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
+        }
+        &:active {
+            box-shadow: inset 0px 0px 5px rgba(0, 0, 0, 0.5);
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="container">
         <top-bar />
+        <header-section />
     </div>
 </template>
 
 <script>
 import TopBar from '@/components/TopBar';
+import HeaderSection from '@/components/HeaderSection';
 
 export default {
     components: {
-        TopBar
+        TopBar,
+        HeaderSection
     }
 };
 </script>
