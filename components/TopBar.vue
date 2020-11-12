38817c9bc4950266c88ace9e8dcf97a629f1fcff
Sarang <sarang98divakaran@gmail.com>
2020-11-12 16:48:27 +0530

    🌟 feat: added topbar component

---
+++ components/TopBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="top-bar">
+        <span class="logo">/NOW_PLAYING.md</span>
+    </div>
+</template>
+
+<style lang="scss">
+.top-bar {
+    padding-top: 1.5rem;
+    padding-bottom: 1rem;
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr;
+    .logo {
+        grid-column: 2;
+        font-size: 0.9rem;
+    }
+}
+</style>
