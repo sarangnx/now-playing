7a5645d448403d031462714d722f075d94d9b28f
Sarang <sarang98divakaran@gmail.com>
2020-11-14 16:05:38 +0530

    🌟 feat: play on spotify link added

--- js/card.js
+++ js/card.js
@@ -31,7 +31,13 @@ export async function renderCard(track) {
             <g clip-path="url(#padding)">
                 <image xlink:href="${dataURI}" height="80" width="80" x="10" y="10" preserveAspectRatio="xMidYMid slice"/>
                 <text x="100" y="20" class="title">${track.name}</text>
-                <text x="100" y="30" class="artist">${artists}</text>
+                <text x="100" y="35" class="artist">${artists}</text>
+                <svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" x="100" y="75">
+                    <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                </svg>
+                <a href="${track.external_urls.spotify}">
+                    <text text-anchor="end" x="290" y="85" class="play">Play on Spotify</text>
+                </a>
             </g>
 
             <defs>
@@ -45,10 +51,15 @@ export async function renderCard(track) {
                     font-family: Ubuntu;
                 }
                 .title {
-                    font-size: 10px;
+                    font-size: 13px;
                 }
                 .artist {
+                    font-size: 10px;
+                    fill: #5b5b5b;
+                }
+                .play {
                     font-size: 8px;
+                    fill: #5b5b5b;
                 }
             </style>
         </svg>`;
