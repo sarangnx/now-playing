5f575503d6bd2a7d59e58f63ce50616b9e76b53d
Sarang <sarang98divakaran@gmail.com>
2020-11-15 13:47:24 +0530

    🌟 feat: add more description and demo image

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,15 @@
             </template>
             <template #content>
                 <p style="display: inline-flex">Hey There,<img src="@/assets/wave.png" class="emoji" /></p>
+                <p>
+                    Dynamically generate svg cards with your current playing track from spotify and add it to your
+                    GitHub README.
+                </p>
+                <p>Here's what I'm listening to right now.</p>
+                <div class="image">
+                    <generated @click="openSpotify" />
+                </div>
+                <p>Click <a href="/api/auth">here</a> to get started.</p>
             </template>
         </readme>
     </div>
@@ -24,6 +33,7 @@ import HeaderSection from '@/components/HeaderSection';
 import Readme from '@/components/Readme';
 import Octocat from '@/assets/octocat.svg';
 import Edit from '@/assets/edit.svg';
+import Generated from '@/assets/generated.svg';
 
 export default {
     components: {
@@ -31,7 +41,13 @@ export default {
         HeaderSection,
         Readme,
         Octocat,
-        Edit
+        Edit,
+        Generated
+    },
+    methods: {
+        openSpotify() {
+            window.open('https://open.spotify.com/track/0ytvsZOerGzUWfHXVT2Sgy', '_blank');
+        }
     }
 };
 </script>
