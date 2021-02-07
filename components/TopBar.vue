5860ddaa7e980c8c5d602dcaa32600ff3f9703a5
Sarang <sarang98divakaran@gmail.com>
2021-02-07 13:56:08 +0530

    🌟 feat: Added hover effects to my code button

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="top-bar">
         <a @click="$router.push('/')" class="logo">/NOW_PLAYING.md</a>
-        <a v-if="hasCode" @click="$router.push('/code')" class="code">Get your code</a>
+        <a v-if="hasCode" @click="$router.push('/code')" class="code">MY CODE</a>
     </div>
 </template>
 
@@ -22,6 +22,7 @@ export default {
     padding-bottom: 1rem;
     display: grid;
     grid-template-columns: 1fr 5fr 5fr 1fr;
+    align-items: center;
     a {
         cursor: pointer;
         font-size: 0.9rem;
@@ -32,6 +33,16 @@ export default {
         &.code {
             grid-column: 3;
             justify-self: end;
+            background: white;
+            color: black;
+            padding: 0.4rem 1.5rem;
+            border-radius: 30px;
+            border: 1px solid black;
+            &:hover,
+            &:active {
+                background: black;
+                color: white;
+            }
         }
     }
 }
