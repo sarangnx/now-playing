0ee65bc28db3653272544efdf89312bb80412ee7
Sarang <sarang98divakaran@gmail.com>
2021-02-07 15:54:48 +0530

    🌟 feat: sanitize track name and artist name for xml

--- js/card.js
+++ js/card.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import escape from 'lodash.escape';
 
 /**
  * Render now playing svg card
@@ -30,8 +31,8 @@ export async function renderCard(track) {
             <rect x="0" y="0" width="300" height="100" fill="white" stroke="#bfbfbf" rx="5"  ry="5"/>
             <g clip-path="url(#padding)">
                 <image xlink:href="${dataURI}" height="80" width="80" x="10" y="10" preserveAspectRatio="xMidYMid slice"/>
-                <text x="100" y="20" class="title">${track.name}</text>
-                <text x="100" y="35" class="artist">${artists}</text>
+                <text x="100" y="20" class="title">${escape(track.name)}</text>
+                <text x="100" y="35" class="artist">${escape(artists)}</text>
                 <svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" x="100" y="75">
                     <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                 </svg>
